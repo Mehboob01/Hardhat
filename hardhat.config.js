5068c628c820afd97aa2c1339b43dd780f3e0d59
Mehboob01 <mehboobelahi661@gmail.com>
2023-05-22 13:13:59 +0500

    change .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config(); // Import the dotenv package
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -9,19 +11,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-const metamask_private_key = "8653f759523a1c0539685b20f2f2b29edb1d3ee07988bb4c2b05408cf1326624";
-const etherscanAPIKey = "D82KIQMEPFRKUBZVXAY7A99UDNJ847A8H6";
+const privateKey = process.env.BSC_PRIVATE_KEY; // Retrieve the private key from the .env file
+const etherscanAPIKey = process.env.ETHERSCAN_API_KEY; // Retrieve the Etherscan API key from the .env file
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
   networks: {
     bscTestnet: {
-      url: "https://data-seed-prebsc-2-s3.binance.org:8545",
-      accounts: [metamask_private_key],
+      url: 'https://data-seed-prebsc-2-s3.binance.org:8545',
+      accounts: [privateKey],
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/PROJECT_ID",
-      accounts: [metamask_private_key],
+      url: 'https://mainnet.infura.io/v3/PROJECT_ID',
+      accounts: [privateKey],
     },
   },
   etherscan: {
