592c5b54a81d0f36116fa75e3f46c20c345cedfd
Mehboob01 <mehboobelahi661@gmail.com>
2023-05-23 14:48:57 +0500

    constructor address add

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers, hre } = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -6,26 +6,26 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
+ 
+  // pass value as a constructor
+  const token = "100000";
+  
+  const SimpleToken = await ethers.getContractFactory("SimpleToken");
+  const simpleToken = await SimpleToken.deploy(token);
+  await simpleToken.deployed();
 
-  const token="0xfCacB1e616F0Aa55378a68fb3A815444CFF9f9fc";
-  const defaultReferrer="0xb5fc14ee4DBA399F9043458860734Ed33FdCd96E";
+  console.log("Contract address:", simpleToken.address);
 
-  const BigDaddy = await ethers.getContractFactory("BigDaddy");
-  const bigDaddy = await BigDaddy.deploy(token,defaultReferrer);
-  await bigDaddy.deployed();
-
-  console.log("Contract address:", bigDaddy.address);
-
-  if (hre.network.name !== "hardhat") {
-    console.log("Verifying contract on Etherscan...");
+  if (network.name !== "hardhat") {
+    console.log("Verifying contract on BSC Testnet Network...");
     try {
-      await hre.run("verify:verify", {
-        address: bigDaddy.address,
-        constructorArguments: [token, defaultReferrer], // Pass the actual values here
+      await run("verify:verify", {
+        address: simpleToken.address,
+        constructorArguments: [token], // Pass the actual values here
       });
-      console.log("Contract verified on Etherscan!");
+      console.log("Contract verified on BSC Testnet Network!");
     } catch (error) {
-      console.error("Failed to verify contract on Etherscan:", error);
+      console.error("Failed to verify contract on BSC Testnet Network:", error);
     }
   }
 }
