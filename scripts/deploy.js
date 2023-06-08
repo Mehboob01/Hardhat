b849b5119aa376f0de6b97aa74ef9ce65776b18d
Mehboob01 <mehboobelahi661@gmail.com>
2023-06-08 12:45:52 +0500

    final

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,19 +8,19 @@ async function main() {
   console.log("Account balance:", (await deployer.getBalance()).toString());
  
   // pass value as a constructor
-  const token = "100000";
+  const token = "0xfCacB1e616F0Aa55378a68fb3A815444CFF9f9fc";
   
-  const SimpleToken = await ethers.getContractFactory("SimpleToken");
-  const simpleToken = await SimpleToken.deploy(token);
-  await simpleToken.deployed();
+  const AirDrop = await ethers.getContractFactory("AirDrop");
+  const airDrop = await AirDrop.deploy(token);
+  await airDrop.deployed();
 
-  console.log("Contract address:", simpleToken.address);
+  console.log("Contract address:", airDrop.address);
 
   if (network.name !== "hardhat") {
     console.log("Verifying contract on BSC Testnet Network...");
     try {
       await run("verify:verify", {
-        address: simpleToken.address,
+        address: airDrop.address,
         constructorArguments: [token], // Pass the actual values here
       });
       console.log("Contract verified on BSC Testnet Network!");
