54ce322ac3094b6fbd4f6f7216700a3e0cc8becb
Mehboob01 <mehboobelahi661@gmail.com>
2023-06-14 17:17:27 +0500

    change deploy file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,68 @@
-const { ethers } = require("hardhat");
+const { ethers, run } = require("hardhat");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  try {
+    const [deployer] = await ethers.getSigners();
+    const network = await ethers.provider.getNetwork();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+    console.log("Deploying contracts with the account:", deployer.address);
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
- 
-  // pass value as a constructor
-  const token = "0xfCacB1e616F0Aa55378a68fb3A815444CFF9f9fc";
-  
-  const AirDrop = await ethers.getContractFactory("AirDrop");
-  const airDrop = await AirDrop.deploy(token);
-  await airDrop.deployed();
+    const balance = await deployer.getBalance();
+    const balanceInEther = ethers.utils.formatEther(balance);
+    console.log("Account balance:", balanceInEther);
 
-  console.log("Contract address:", airDrop.address);
+    const block = await ethers.provider.getBlock(await ethers.provider.getBlockNumber());
+    const timestamp = block.timestamp;
+    const date = new Date(timestamp * 1000);
 
-  if (network.name !== "hardhat") {
-    console.log("Verifying contract on BSC Testnet Network...");
-    try {
-      await run("verify:verify", {
-        address: airDrop.address,
-        constructorArguments: [token], // Pass the actual values here
-      });
-      console.log("Contract verified on BSC Testnet Network!");
-    } catch (error) {
-      console.error("Failed to verify contract on BSC Testnet Network:", error);
+    const formattedDate = date.toLocaleString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZoneName: "short",
+    });
+
+    console.log("Real-time Timestamp:", formattedDate);
+
+    const blockchainInfo = {
+      name: network.name,
+      chainId: network.chainId,
+      blockNumber: block.number,
+      timestamp: timestamp,
+    };
+
+    console.log("Blockchain Info:");
+    console.table(blockchainInfo);
+
+    const Owership = await ethers.getContractFactory("Owership");
+    const token = await Owership.deploy('0xb5fc14ee4DBA399F9043458860734Ed33FdCd96E');
+    await token.deployed();
+
+    console.log("Contract address:", token.address);
+
+    if (network.name !== "hardhat") {
+      console.log("Verifying contract on the network...");
+      await verifyContract(token.address);
+      console.log("Contract verified on the network!");
     }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+async function verifyContract(contractAddress) {
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: ['0xb5fc14ee4DBA399F9043458860734Ed33FdCd96E'], // Pass the actual values here
+    });
+  } catch (error) {
+    console.error("Failed to verify contract on the network:", error);
+    throw error;
   }
 }
 
